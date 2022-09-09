c7ef003bc120e8d854499be07cacf9d973deb1ce
iroderick00 <iroderick@outlook.es>
2022-09-09 18:40:33 -0300

    primer commit

---
+++ LogTest/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using NLog;
+
+public static class Program
+{
+    private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+    public static void Main()
+    {
+        Console.WriteLine("Hello world!");
+        Logger.Info("Goodbye cruel world");
+        Logger.Debug("Goodbye cruel world");
+    }
+}  
---
+++ README.md
@@ -0,0 +1,5 @@
+### Taller de Lenguajes II - 2022
+
+# Trabajo Práctico N° 2 
++ Manejo de Try - Catch
++ Manejo de NLog
