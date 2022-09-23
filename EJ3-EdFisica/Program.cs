5fdc8f9b036922aea88f78d823192a9d244ea9dd
juani981 <myemail>
2022-09-23 15:47:15 -0300

    rlo

--- EJ3-EdFisica/Program.cs
+++ EJ3-EdFisica/Program.cs
@@ -7,6 +7,23 @@ public static class Program
     private static void Main(string[] args)
     {
         Logger.Trace("Inicio de ejecución del programa");
+        //Plantilla menu principal:
+        string? opcion;
+        do {
+            Console.Clear();
+            Console.WriteLine("\n¿Qué operación desea realizar?\n");
+            Console.WriteLine("(I)ngresar Alumno    (B)orrar    (S)alir\n");
+            try {
+                opcion = Console.ReadLine()!.ToLower();
+            }
+            catch (Exception) {
+                opcion = "z";
+            }
+            if (opcion == "i")
+                cargarLista();
+            if (opcion == "b")
+                borrarLista();
+        } while (opcion != "s");
 
     }
 }
